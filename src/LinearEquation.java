99c9b46cf9519f66b48b3c0acfbb6670394516cc
student <bonniec15@nycstudents.net>
2022-10-17 10:57:46 -0400

    Finished with slope method

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,8 +1,15 @@
 public class LinearEquation {
-    private double slope;
+
     public LinearEquation() {
     }
 
+    public double slope(int coord2Y, int coord1Y, int coord2X, int coord1X) {
+        int rise = coord2Y - coord1Y;
+        int run = coord2X - coord1X;
+        double slope = rise/run;
+
+        return slope;
+    }
     public String equation() {
 
         return "The equation of the line between these two points is: y = ";
